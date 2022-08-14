831ce94cdb41302248ef8dbb59b1e9ca0cd9c466
Rajiv Kumar <ecominds7@gmail.com>
2022-08-14 22:45:56 +0530

    Initial commit for working code of microservice with h2 database

---
+++ dept-api-with-h2/README.md
@@ -0,0 +1,2 @@
+# boot-api-with-h2
+A simple microservice with springboot and h2 database 
---
+++ dept-api-with-h2/src/main/java/com/github/rajiv7/ms/api/department/DeptServiceStarterClass.java
@@ -0,0 +1,23 @@
+package com.github.rajiv7.ms.api.department;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class DeptServiceStarterClass implements CommandLineRunner {
+    @Autowired
+    private Environment environment;
+    public static void main(String[] args) {
+        SpringApplication.run(DeptServiceStarterClass.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        for (String profileName : environment.getActiveProfiles()) {
+            System.out.println("Currently active profile - " + profileName);
+        }
+    }
+}
