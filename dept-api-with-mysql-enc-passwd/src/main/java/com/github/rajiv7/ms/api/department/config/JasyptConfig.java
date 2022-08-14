b3adf987a15870c6346339861c6cf12a643e52c7
Rajiv Kumar <ecominds7@gmail.com>
2022-08-15 02:28:13 +0530

    Working code of microservice with mysql encrypted password and externalized configuration file

--- dept-api-with-mysql-enc-passwd/src/main/java/com/github/rajiv7/ms/api/department/config/JasyptConfig.java
+++ dept-api-with-mysql-enc-passwd/src/main/java/com/github/rajiv7/ms/api/department/config/JasyptConfig.java
@@ -3,16 +3,21 @@ package com.github.rajiv7.ms.api.department.config;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JasyptConfig {
-	public static SimpleStringPBEConfig getSimpleStringPBEConfig() {
+	
+	@Value ("${env.secret.pwd}")
+	private String pwd;
+	
+	@Value ("${env.secret.algo}")
+	private String algo;
+	
+	private static SimpleStringPBEConfig getSimpleStringPBEConfig(String pwd, String algo) {
         final SimpleStringPBEConfig pbeConfig = new SimpleStringPBEConfig();
-        /**
-         *  the below hard coded configuration can be load from the environment properties
-         */
         pbeConfig.setPassword("supersecretz");
         pbeConfig.setAlgorithm("PBEWithMD5AndDES");
         pbeConfig.setKeyObtentionIterations("1000");
@@ -27,8 +32,7 @@ public class JasyptConfig {
 	@Bean(name = "jasyptStringEncryptor")
     public StringEncryptor encryptor() {
         final PooledPBEStringEncryptor pbeStringEncryptor = new PooledPBEStringEncryptor();
-        pbeStringEncryptor.setConfig(getSimpleStringPBEConfig());
- 
-        return pbeStringEncryptor;
+        pbeStringEncryptor.setConfig(getSimpleStringPBEConfig(pwd, algo));
+         return pbeStringEncryptor;
     }
 }
